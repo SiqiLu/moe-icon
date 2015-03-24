c765b41b09123167705541e349705a06ea2c9de7
Siqi Lu <lu.siqi@outlook.com>
2015-03-24 18:24:33 +0800

    sync with moe-static

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,129 +1,140 @@
-﻿/* !
- * moe UI Gruntfile
- * http://ui.moe.click
- * Copyright 2013-2015 Siqi Lu
- * Licensed under MIT
- */
+﻿module.exports = function (grunt) {
+  'use strict';
 
-module.exports = function(grunt) {
-    'use strict';
+  grunt.initConfig({
+    clean: {
+      dist: 'dist',
+      npm: 'node_modules'
+    },
 
-    grunt.initConfig({
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      grunt: {
+        src: 'Gruntfile.js'
+      },
+      core: {
+        src: ['js/*.js', '!js/*.min.js']
+      }
+    },
 
-        clean: {
-            dist: 'dist',
-            npm: 'node_modules'
-        },
+    jscs: {
+      options: {
+        config: '.jscsrc'
+      },
+      grunt: {
+        src: '<%= jshint.grunt.src %>'
+      },
+      core: {
+        src: '<%= jshint.core.src %>'
+      }
+    },
 
-        jshint: {
-            options: {
-                jshintrc: '.jshintrc'
-            },
-            grunt: {
-                src: 'Gruntfile.js'
-            },
-            core: {
-                src: ['js/*.js', '!js/*.min.js']
-            }
-        },
+    uglify: {
+      options: {
+        preserveComments: 'all',
+        sourceMap: true
+      },
+      core: {
+        src: 'js/moe-icon-ie7.js',
+        dest: 'js/moe-icon-ie7.min.js'
+      }
+    },
 
-        jscs: {
-            options: {
-                config: '.jscsrc'
-            },
-            grunt: {
-                src: '<%= jshint.grunt.src %>'
-            },
-            core: {
-                src: '<%= jshint.core.src %>'
-            }
-        },
+    csslint: {
+      options: {
+        csslintrc: '.csslintrc'
+      },
+      core: {
+        src: 'css/moe-icon.css'
+      }
+    },
 
-        uglify: {
-            options: {
-                preserveComments: 'all',
-                sourceMap: true
-            },
-            core: {
-                src: 'js/moe-icon-ie7.js',
-                dest: 'js/moe-icon-ie7.min.js'
-            }
-        },
+    csscomb: {
+      options: {
+        config: '.csscomb.json'
+      },
+      core: {
+        expand: true,
+        cwd: 'css/',
+        src: '*.css',
+        dest: 'css/'
+      }
+    },
 
-        csslint: {
-            options: {
-                csslintrc: '.csslintrc'
-            },
-            core: {
-                src: 'css/moe-icon.css'
-            }
-        },
+    cssmin: {
+      options: {
+        compatibility: 'ie8',
+        keepSpecialComments: '0',
+        advanced: true
+      },
+      core: {
+        files: [{
+          expand: true,
+          cwd: 'src/css',
+          src: ['*.css', '!*.min.css'],
+          dest: 'src/css',
+          ext: '.min.css'
+        }]
+      }
+    },
 
-        csscomb: {
-            options: {
-                config: '.csscomb.json'
-            },
-            core: {
-                expand: true,
-                cwd: 'css/',
-                src: '*.css',
-                dest: 'css/'
-            }
-        },
+    copy: {
+      release: {
+        expand: true,
+        src: ['fonts/**', 'css/moe-*', 'js/moe-*'],
+        dest: 'dist/'
+      }
+    },
 
-        cssmin: {
-            options: {
-                compatibility: 'ie8',
-                keepSpecialComments: '0',
-                advanced: true
-            },
-            core: {
-                files: [{
-                    expand: true,
-                    cwd: 'src/css',
-                    src: ['*.css', '!*.min.css'],
-                    dest: 'src/css',
-                    ext: '.min.css'
-                }]
-            }
-        },
+    exec: {
+      options: {
+        stdout: true,
+        stderr: true
+      },
+      npmUpdate: {
+        command: 'npm update'
+      },
+      npmInstall: {
+        command: 'npm install'
+      },
+      npmPublish: {
+        command: 'npm publish'
+      }
+    },
 
-        copy: {
-            release: {
-                expand: true,
-                src: ['fonts/**', 'css/moe-*', 'js/moe-*'],
-                dest: 'dist/'
-            }
-        },
+    bump: {
+      options: {
+        files: ['package.json', 'bower.json'],
+        updateConfigs: [],
+        commit: true,
+        commitMessage: 'Release %VERSION%',
+        commitFiles: ['-a'],
+        createTag: true,
+        tagName: '%VERSION%',
+        tagMessage: 'Version %VERSION%',
+        push: true,
+        pushTo: 'origin master',
+        gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+        globalReplace: false
+      }
+    }
+  });
 
-        bump: {
-            options: {
-                files: ['package.json', 'bower.json'],
-                updateConfigs: [],
-                commit: true,
-                commitMessage: 'Release %VERSION%',
-                commitFiles: ['-a'],
-                createTag: true,
-                tagName: '%VERSION%',
-                tagMessage: 'Version %VERSION%',
-                push: true,
-                pushTo: 'origin master',
-                gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
-                globalReplace: false
-            }
-        }
-    });
+  // This command registers the default task which will install bower packages into wwwroot/lib
+  grunt.registerTask('default', ['js', 'css', 'build']);
 
-    // This command registers the default task which will install bower packages into wwwroot/lib
-    grunt.registerTask('default', ['js', 'css', 'build']);
+  grunt.registerTask('js', ['jshint', 'jscs', 'uglify']);
+  grunt.registerTask('css', ['csslint', 'csscomb', 'cssmin']);
+  grunt.registerTask('build', ['clean:dist', 'copy']);
+  grunt.registerTask('release', ['exec:npmUpdate', 'default', 'bump']);
+  grunt.registerTask('release-minor', ['exec:npmUpdate', 'default', 'bump:minor']);
+  grunt.registerTask('release-major', ['exec:npmUpdate', 'default', 'bump:major']);
 
-    grunt.registerTask('js', ['jshint', 'jscs', 'uglify']);
-    grunt.registerTask('css', ['csslint', 'csscomb', 'cssmin']);
-    grunt.registerTask('build', ['clean:dist', 'copy']);
-    grunt.registerTask('release', ['bump']);
-    // These plugins provide necessary tasks.
-    require('load-grunt-tasks')(grunt, {
-        scope: 'devDependencies'
-    });
-    require('time-grunt')(grunt);
+  // These plugins provide necessary tasks.
+  require('load-grunt-tasks')(grunt, {
+    scope: 'devDependencies'
+  });
+  require('time-grunt')(grunt);
 };
